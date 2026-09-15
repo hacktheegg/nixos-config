3316c3ef761e1fd07c7942b094de8e818bdb6647
hacktheegg <hacktheegg@hacktheegg@hacktheegg@hacktheegg>
2026-09-15 20:54:26 +1000

    key changed again

--- Builds/Practice-Server.nix
+++ Builds/Practice-Server.nix
@@ -199,7 +199,7 @@
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG3G3MXV0lULAAMHHR5vj8rOD+9mc/jAuvbbKOQ/jTrH agenix recovery"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF+cXDNU7PAa7gxV+1iZ2+agsxEE2T9FAIOHjwrIvx+9 trmwdc@gmail.com"
         "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM5ObBl3+9+6RsvhLo0SvBwZISP8MZ7I4VDBKIE+Se18 marley@marley-laptop-mint"
-        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID3BNJjHcXlRiDqFBNaPHh6TXEifMnL205j11HpjZDE+ doubleabattery"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE+WPY9B4z8mRavi7xNMPiV++SZVKHzlnBxoSuggA1UN nedaa@Computer"
       ];
       initialPassword = "abc";
     };
